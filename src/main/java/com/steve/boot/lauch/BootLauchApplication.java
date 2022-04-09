e2d635c0c3945a114cf3503dead81b822ee656fe
Steve <steve.yang.au@gmail.com>
2022-04-09 19:17:45 +1000

    set Lombok and spring.boot.devtools up and learn how to use it.

--- src/main/java/com/steve/boot/lauch/BootLauchApplication.java
+++ src/main/java/com/steve/boot/lauch/BootLauchApplication.java
@@ -1,5 +1,7 @@
 package com.steve.boot.lauch;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.steve.boot.lauch.model.LombokPOJO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootLauchApplication {
 
     public static void main(String[] args) {
+        LombokPOJO lombokPOJO = LombokPOJO.builder().name("Steve").age(22).build();
+        System.out.println(lombokPOJO);
         SpringApplication.run(BootLauchApplication.class, args);
     }
 
---
+++ src/main/java/com/steve/boot/lauch/model/LombokPOJO.java
@@ -0,0 +1,19 @@
+package com.steve.boot.lauch.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Slf4j
+@Builder
+public class LombokPOJO {
+
+    private String name;
+
+    private int age;
+}
