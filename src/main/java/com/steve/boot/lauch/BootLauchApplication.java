ae804b05399df86f216ce5f81581fcd20764ea0f
Steve <steve.yang.au@gmail.com>
2022-04-09 19:57:36 +1000

    Codota                           autocomplete java source code Auto filling Java call arguments GsonFormat   json string -> java entity (alt + s) Rainbow Brackets Maven Helper
    learn plugins above

--- src/main/java/com/steve/boot/lauch/BootLauchApplication.java
+++ src/main/java/com/steve/boot/lauch/BootLauchApplication.java
@@ -5,12 +5,11 @@ import com.steve.boot.lauch.model.LombokPOJO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class BootLauchApplication {
-
     public static void main(String[] args) {
-        LombokPOJO lombokPOJO = LombokPOJO.builder().name("Steve").age(22).build();
-        System.out.println(lombokPOJO);
         SpringApplication.run(BootLauchApplication.class, args);
     }
 
---
+++ src/main/java/com/steve/boot/lauch/model/SuperHero.java
@@ -0,0 +1,81 @@
+package com.steve.boot.lauch.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Data
+public class SuperHero {
+    /**
+     * {
+     *   "squadName": "Super hero squad",
+     *   "homeTown": "Metro City",
+     *   "formed": 2016,
+     *   "secretBase": "Super tower",
+     *   "active": true,
+     *   "members": [
+     *     {
+     *       "name": "Molecule Man",
+     *       "age": 29,
+     *       "secretIdentity": "Dan Jukes",
+     *       "powers": [
+     *         "Radiation resistance",
+     *         "Turning tiny",
+     *         "Radiation blast"
+     *       ]
+     *     },
+     *     {
+     *       "name": "Madame Uppercut",
+     *       "age": 39,
+     *       "secretIdentity": "Jane Wilson",
+     *       "powers": [
+     *         "Million tonne punch",
+     *         "Damage resistance",
+     *         "Superhuman reflexes"
+     *       ]
+     *     },
+     *     {
+     *       "name": "Eternal Flame",
+     *       "age": 1000000,
+     *       "secretIdentity": "Unknown",
+     *       "powers": [
+     *         "Immortality",
+     *         "Heat Immunity",
+     *         "Inferno",
+     *         "Teleportation",
+     *         "Interdimensional travel"
+     *       ]
+     *     }
+     *   ]
+     * }
+     */
+
+    @JsonProperty("squadName")
+    private String squadName;
+    @JsonProperty("homeTown")
+    private String homeTown;
+    @JsonProperty("formed")
+    private Integer formed;
+    @JsonProperty("secretBase")
+    private String secretBase;
+    @JsonProperty("active")
+    private Boolean active;
+    @JsonProperty("members")
+    private List<MembersDTO> members;
+
+    @NoArgsConstructor
+    @Data
+    public static class MembersDTO {
+        @JsonProperty("name")
+        private String name;
+        @JsonProperty("age")
+        private Integer age;
+        @JsonProperty("secretIdentity")
+        private String secretIdentity;
+        @JsonProperty("powers")
+        private List<String> powers;
+    }
+}
