02826b0ff1f3cfeeeb2ebee90ab09e1a0757aba5
Steve <steve.yang.au@gmail.com>
2022-04-17 04:19:23 +1000

    finish renaming

--- src/test/java/com/steve/boot/launch/BootLauchApplicationTests.java
+++ src/test/java/com/steve/boot/launch/BootLauchApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BootLauchApplicationTests {
+class BootLaunchApplicationTests {
 
     @Test
     void contextLoads() {
