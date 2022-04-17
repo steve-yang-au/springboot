6c49606b087963b107f5658021db00a07b710ea3
Steve <steve.yang.au@gmail.com>
2022-04-17 19:06:39 +1000

    use hibernate-validator to validate beans when binding properties

--- src/main/java/com/steve/boot/launch/model/Father.java
+++ src/main/java/com/steve/boot/launch/model/Father.java
@@ -2,8 +2,21 @@ package com.steve.boot.launch.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+
 @Data
 public class Father {
     private String name;
+    @Min(21)
     private int age;
 }
+/**
+ * more validation annotations to use:
+ * @Size
+ * @Patten
+ * @Length
+ * @Email
+ * @NotNull
+ * @Max
+ * @Min
+ */
